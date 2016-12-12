9a9c16938db8e13219b3ac1836a2e5ffd13b6ea6
Todd <tpolasek@gmail.com>
2016-12-12 15:06:55 -0800

    created Countdown Form Component and tests

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ var LiveReloadPlugin = require('webpack-livereload-plugin');
        Timer: 'app/components/Timer.jsx',
        Countdown: 'app/components/Countdown.jsx',
        Clock: 'app/components/Clock.jsx',
+       CountdownForm: 'app/components/CountdownForm.jsx',
        applicationStyles: 'app/styles/app.scss'
      },
      extensions: ['', '.js', '.jsx']
