1c09666511eee6e3134e5dcc83c48fd6d5dd45e1
Todd <tpolasek@gmail.com>
2016-12-12 13:51:51 -0800

    added clock component with component testing

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ var LiveReloadPlugin = require('webpack-livereload-plugin');
        Nav: 'app/components/Nav.jsx',
        Timer: 'app/components/Timer.jsx',
        Countdown: 'app/components/Countdown.jsx',
+       Clock: 'app/components/Clock.jsx',
        applicationStyles: 'app/styles/app.scss'
      },
      extensions: ['', '.js', '.jsx']
